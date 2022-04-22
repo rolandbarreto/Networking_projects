ebc1292da8e24047afad37552a6be3c0cc475137
rolandbarreto <rolandbarreto19@gmail.com>
2022-04-22 10:58:59 -0600

    Added new two functions that calculates the mask and the type of Ip address

--- SubnetCalculator/subnet_calculator.py
+++ SubnetCalculator/subnet_calculator.py
@@ -1,3 +1,6 @@
+import ipaddress
+
+
 def address_type(address):
     address_ip = address.split('.')
     first_octet = int(address_ip[0])
@@ -9,11 +12,45 @@ def address_type(address):
         return 'C'
     else:
         return 'Unknown'
+
+
+def address_mask(address_class):
+    if address_class == 'A':
+        return '255.0.0.0'
+    elif address_class == 'B':
+        return '255.255.0.0'
+    elif address_class == 'C':
+        return '255.255.255.0'
+    else:
+        return 'Unknown'
+
+
+def get_ip_type(ip):
+    if ip.is_private:
+        return 'Privada'
+    elif ip.is_loopback:
+        return 'Loopback'
+    elif ip.is_multicast:
+        return 'Multicast'
+    elif ip.is_reserved:
+        return 'Reservada'
+    elif not ip.is_private:
+        return 'Publica'
+    else:
+        return 'Unknown'
     
 
 def run():
     address = input('Ingrese su dirección IP: ')
-    print('Esta dirección es de tipo: ' + address_type(address))
+    address_class = address_type(address)
+    mask = address_mask(address_class)
+    print('Esta dirección es de tipo: ' + address_class)
+    print('Esta es su mascara correspondiente: ' + mask)
+    
+
+    ip = ipaddress.IPv4Address(address)
+    print('Esta es el tipo: ' + get_ip_type(ip))
+
 
 
 if __name__ == '__main__':
